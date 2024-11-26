ba173a806a08bd9d9f858c3b0fedf8af28fae513
Eduardo Miranda <102879023+sousax@users.noreply.github.com>
2024-11-26 15:02:12 -0300

    Add files via upload

---
+++ ProjBiblioteca/Livros.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjBiblioteca
+{
+    internal class Livros
+    {
+        private List<Livro> acervo;
+        public List<Livro> Acervo { get => acervo; }
+
+        public Livros()
+        {
+            acervo = new List<Livro>();
+        }
+        public void adicionar(Livro Livro)
+        {
+            if (Livro != null && !Acervo.Contains(Livro))
+            {
+                acervo.Add(Livro);
+            }
+        }
+
+        public Livro pesquisar(Livro livro)
+        {
+            return acervo.FirstOrDefault(l => l.Isbn == livro.Isbn);
+        }
+
+    
+}
+}
